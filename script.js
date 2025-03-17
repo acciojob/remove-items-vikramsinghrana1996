97f4d0c3cb0209d392cc0d4a2814f4560f5a1fb1
vikramsinghrana1996 <63793249+vikramsinghrana1996@users.noreply.github.com>
2025-03-18 04:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+function removeColor() {
+    let colorDropdown = document.getElementById("colorSelect");
+    if (colorDropdown.selectedIndex !== -1) {
+        colorDropdown.remove(colorDropdown.selectedIndex);
+    }
+}
